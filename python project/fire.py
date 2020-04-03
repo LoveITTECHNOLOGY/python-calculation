852a75dc5051e85ddb66fbd4c99ce753147859f8
weijiafu19981002 <62496045+weijiafu19981002@users.noreply.github.com>
2020-04-03 23:55:23 +0800

    Update fire.py

--- python project/fire.py
+++ python project/fire.py
@@ -1,17 +1,14 @@
 from pyphysicssandbox import *
 from random import randint
 
-
-
 def obver_func(keys):
-    """key是按键，加计数器每一秒运行一次函数，"""
     rgb=Color(randint(0,255),randint(0,255),randint(0,255))
     p=ColoredParticle(rgb)
     p.hit()
 class Particle:
     def __init__(self):
         self.ball=ball((200+randint(-1,1),200+randint(-1,1)),20)
-        self.direction=randint(-2000,2000),randint(-3000,3000)
+        self.direction=randint(-2000,2000),randint(-4000,4000)
     def hit(self):
         self.ball.hit(self.direction,(0,0))
 class ColoredParticle(Particle):
@@ -20,9 +17,8 @@ class ColoredParticle(Particle):
         self.color=c
         self.ball.color=self.color
 
-window("python window",400,400)
+window("fire",400,400)
 c=cosmetic_box((0,0,0),400,400)
 c.color=(0,0,0)
-
-obver_func(add_observer)
+add_observer(obver_func)
 run()
